a49c88635e7c699d5d97d900a3d994aed8bc5655
javier.menga <jfm94@hotmail.com>
2021-11-25 00:23:18 -0300

    component CartWidget fixed

--- src/components/CartWidget.js
+++ src/components/CartWidget.js
@@ -5,12 +5,12 @@ import cart from './multimedia/cart.png'
 const CartWidget = () => {
     return (
         <div className="d-grid gap-2 d-md-flex justify-content-md-end">
-            <button className="btn btn-light me-md-2" type="button">
-                <img src={cart} alt="cart" style={{widht:25,height:30}}/>
+            <button className="btn btn-light me-md-2 mt-2" type="button" style={{display:'flex', flexDirection:'row'}}>
+                <img src={cart} alt="cart" style={{widht:25,height:30,}}/>
                 <p style={{paddingLeft:10}}>4</p>
             </button>
         </div>
     )
 }
 
-export default CartWidget;
+export default CartWidget;  
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,8 +4,8 @@ import CartWidget from './CartWidget';
 const NavBar = () => {
     return ( <nav className = "navbar navbar-expand-lg navbar-light bg-light">
                 <div className = "container-fluid">
-                    <a className = "navbar-brand" href = "#">
-                        <img src={brand} className="brand" />
+                    <a className = "navbar-brand" href = "/#">
+                        <img src={brand} className="brand" alt="brand" />
                     </a>
                     <button className = "navbar-toggler" type = "button" data-bs-toggle = "collapse" data-bs-target = "navbarNav" aria-controls = "navbarNav" aria-expanded = "false" aria-label = "Toggle navigation">
                         <span className = "navbar-toggler-icon"> </span>
@@ -13,13 +13,13 @@ const NavBar = () => {
                     <div className = "collapse navbar-collapse" id = "navbarNav">
                         <ul className = "navbar-nav ms-5 w-100">
                             <li className = "nav-item ms-5">
-                                <a className = "nav-link active" aria-current = "page" href = "#"> Inicios </a>
+                                <a className = "nav-link active" aria-current = "page" href = "/#"> Inicios </a>
                             </li>
                             <li className = "nav-item ms-5">
-                                <a className = "nav-link" href = "#" > Servicios </a>
+                                <a className = "nav-link" href = "/#" > Servicios </a>
                             </li>
                             <li className = "nav-item ms-5">
-                                <a className = "nav-link" href = "#" > Merchandising </a>
+                                <a className = "nav-link" href = "/#" > Merchandising </a>
                             </li>
                             <div className="d-grid gap-2 d-md-flex justify-content-end w-100">
                                 <button className="btn btn-outline-secondary me-md-2" type="button">LOGIN</button>
