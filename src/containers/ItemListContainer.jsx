46ffca6d132a12b3b80260a5600bf2bf8788d13c
javier.menga <jfm94@hotmail.com>
2021-12-01 17:30:20 -0300

    modified ItemCount

--- src/containers/ItemListContainer.jsx
+++ src/containers/ItemListContainer.jsx
@@ -1,11 +1,7 @@
 import ItemCount from "../components/ItemCount/ItemCount";
 
 const ItemListContainer = ({greeting}) => {
-    
-    // const onAdd = () => {
-    //     console.log("Agregado al carrito")
-    // }
-        
+            
     return ( 
         <div style={{display:'flex', flexDirection:"column", alignItems:"center", marginTop:50}}>
             <h1>{greeting}</h1>
